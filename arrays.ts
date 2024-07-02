0bd90a7202078d565697e8ea213e640b3add08b8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-02 17:09:38 -0300

    Tipagem : exemplos de object types

--- arrays.ts
+++ arrays.ts
@@ -13,4 +13,37 @@ const number: Array<number> = [1,2,3,4]
 
 // exemplo de tupla :conjunto de valores que vc sabe que vai  ter dentro de um array.
 
-const pessoa: [string, number]= ["jeff", 35]
+const pessoaTupla: [string, number]= ["jeff", 35]
+
+// ja vem uma tipagem implicita
+const pessoa = {
+    nome: "Jeff",
+    idade: 35, 
+    profissão: "Estudante",
+    altura: 1.77
+}
+
+//object Types
+// utilizando o interface ( associado a orientação objeto) e type é uma coisa mais generica.
+// criando uma modelagem de que é uma pessoa, o nome do atributo vai ser sempre a chave que queremos passar e o valor vai ser o tipo
+interface Person {
+    nome: string,
+    idade: number,
+    profissao?: string, // quando coloco o ? significa que não é obrigatório é opcional 
+    altura:number
+}
+
+type Person = {
+    nome: string,
+    idade: number,
+    profissao: string,
+    altura: number
+}
+
+const pessoaP: Person = {
+    nome: "Jeff",
+    idade: 35, 
+    profissao: "Estudante",
+    altura: 1.77
+}
+
