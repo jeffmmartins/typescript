e570b2136f606ed95621287cab4b6546afc29b8e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-02 16:54:33 -0300

    Tipagem: exemplo de tipagem em tupla

--- arrays.ts
+++ arrays.ts
@@ -1,3 +1,16 @@
 let fruitList: string[] = ["maca", "banana", "mamao"];
 let listaDeFrutas = [] as string[];
 fruitList =[...fruitList]
+
+//sintaxe 
+const numeros: number[] = [1,2,3,4,6]
+
+// mix de tipos de dados dentro do array nao é recomendavel 
+const mix: (number | string )[] = ["jeff", 2, 2.12]
+
+// sintaxe mais antiga 
+const number: Array<number> = [1,2,3,4]
+
+// exemplo de tupla :conjunto de valores que vc sabe que vai  ter dentro de um array.
+
+const pessoa: [string, number]= ["jeff", 35]
--- tipagem.ts
+++ tipagem.ts
@@ -1,4 +1,4 @@
-let numero  =  20; // inferencia de tipo 
+let number  =  20; // inferencia de tipo 
 
 const pi = 3.1415;
 
@@ -7,4 +7,13 @@ let nome: string = "Jeff"
 let correta: boolean;
 
 correta = true
+
+let resultado = number * pi
+
+const dadosDoUsuario = prompt("qual seu nome?")
+
+// ? - significa se o valor for valido faz um tolowercase , se for null ele nao vai aplicar.
+// ajudando a evitar um bug no código.
+console.log(dadosDoUsuario?.toLowerCase)
+
 
