391164be54041a990fda93b02a029929f4ab5c52
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-03 17:46:49 -0300

    POO: criando uma class aplicando typescript

--- poo/dist/index.js
+++ poo/dist/index.js
@@ -1,12 +1,7 @@
+"use strict";
 // Aula 19 - POO
 // escopo da nossa classe - abstração 
 // definindo abstração de uma pessoa. objeto literal é objeto unico 
 // sempre que tiver um obeto que repete muito , fazer a class
-
 class Pessoa {
-    //toda classe tem atributos, sao as caracteristicas 
-    nome: string;
-    idade: number;
-    altura: number;
-    // metodos 
 }
