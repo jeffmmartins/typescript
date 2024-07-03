2f63f2fe1096dabcd98c08b3361aaa8a334d1af4
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-03 20:57:58 -0300

    POO: objeto criado

--- poo/ts/index.ts
+++ poo/ts/index.ts
@@ -4,9 +4,33 @@
 // sempre que tiver um obeto que repete muito , fazer a class
 
 class Pessoa {
-    //toda classe tem atributos, sao as caracteristicas 
+    //toda classe tem atributos, sao as caracteristicas (variaveis)
     nome: string;
     idade: number;
     altura: number;
-    // metodos 
+    // metodos : são ações como sendo uma função 
+
+    //metodo construtor é obrigatório , e pode receber parametros 
+    // esses argumentos tem que atribuir aos atributos criados acima 
+    // this o atributo nome quero que receba o nome que esta sendo passado no momento da cosntrução do objeto
+    constructor(nome: string, idade: number, altura: number ){
+        this.nome = nome
+        this.idade = idade
+        this.altura = altura
+    }
+    
+    dormir(){
+        console.log("dormindo ...")
+    }
 }
+// criando uma pessoa (individuo /objeto) a partir das definições de classe Pessoa ou seja a partir da abstração 
+// o () ao lado de Pessoa representa que estou chamando o constructor da class
+// criar é a mesma coisa de instanciar bastante usado em programação
+// o construtor define o que tenho que passar para a criação de uma nova pessoa  
+//this - este objeto pessoa1 o nome dele seja o que foi passando como paramentro no construtor
+const pessoa1 = new Pessoa("Jeff", 27, 1.70)
+const pessoa2 = new Pessoa("vera", 60, 1.70 )
+
+console.log(pessoa1)
+console.log(pessoa2)
+console.log(pessoa1.dormir())
